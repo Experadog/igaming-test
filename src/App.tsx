a663a86bb0aa77c992ba759ad1923c418b46b7d2
Vlad <lsstudent008@mail.ru>
2026-02-05 00:39:00 +0600

    fix link on game

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,7 @@ const PageWrapper = styled.div`
 const App = () => {
     const { config } = useGeo();
     const [isGameOpen, setIsGameOpen] = useState(false);
-    const gameUrl = `
-        https://gateway.eva-digital-playground.com/v0/casino/games/launch?gameId=n2-novomatic-book-of-ra-deluxe&    channel=desktop&partnerKey=0wl&lobbyUrl=https://chinchincasino.com&mode=demo&language=${config.lang}
-    `;
+    const gameUrl = `https://gateway.eva-digital-playground.com/v0/casino/games/launch?gameId=n2-novomatic-book-of-ra-deluxe&channel=desktop&partnerKey=0wl&lobbyUrl=https://chinchincasino.com&mode=demo&language=${config.lang}`;
 
     return (
         <PageWrapper>
